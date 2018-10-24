a1105494d0ae6641ff009326a9c506bbfafb87d6
shonit-shatani <44223880+shonit-shatani@users.noreply.github.com>
2018-10-24 10:49:47 +0530

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+   n=int(input())
+   if n == 1:
+      return 1
+   elif n == 0:   
+      return 0            
+   else:                      
+      return fibonacci(n-1) + fibonacci(n-2) 
+print (fibonacci())
